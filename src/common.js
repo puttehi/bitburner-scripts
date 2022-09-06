b5a5e5fafde5fe07795b0e729e6ea48142b8760a
puttehi <peteeez95@gmail.com>
2022-09-06 22:40:34 +0300

    fix config again

--- src/common.js
+++ src/common.js
@@ -1,4 +1,4 @@
-import { config } from "config.js"
+import { config } from "./config.js"
 
 const LL = config.logging.levels
 
--- src/config.js
+++ src/config.js
@@ -1,15 +1,13 @@
 export const config = {
-    config: {
-        logging: {
-            level: 1,
-            levels: Object.freeze({
-                TRACE: 0,
-                DEBUG: 1,
-                INFO: 2,
-                WARN: 3,
-                ERROR: 4,
-            }),
-        },
+    logging: {
+        level: 1,
+        levels: Object.freeze({
+            TRACE: 0,
+            DEBUG: 1,
+            INFO: 2,
+            WARN: 3,
+            ERROR: 4,
+        }),
     },
     git_pull: [
         ["github", "puttehi"],
